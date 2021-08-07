bfdb80060c91caf115d31aae520e4613437884a2
Kevin Garner <kevin@kgar.net>
2021-08-07 15:14:31 -0500

    done with chptr 2

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();  // clear screen
 
+        // handle player input
+        player_input(self, ctx);
+
         // get all position components and rederable components in the ECS
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -38,6 +41,11 @@ struct Renderable {
     bg: RGB,
 }
 
+#[derive(Component, Debug)]
+struct Player {
+
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
@@ -52,8 +60,9 @@ fn main() -> rltk::BError {
     // register component types
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<Player>();
 
-    //add initial entity
+    // create player
     gs.ecs.create_entity()
       .with(Position { x: 40, y: 25 })
       .with(Renderable {
@@ -61,6 +70,7 @@ fn main() -> rltk::BError {
           fg: RGB::named(rltk::YELLOW),
           bg: RGB::named(rltk::BLACK),
       })
+      .with(Player {})
       .build();
 
     // add in some test things for now
@@ -77,4 +87,28 @@ fn main() -> rltk::BError {
 
     // loop for-ev-er
     rltk::main_loop(context, gs)
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for(_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // player movement
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
 }

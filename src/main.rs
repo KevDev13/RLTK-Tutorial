6816e8faf83f6b9becc73d7117797273b3824429
Kevin Garner <kevin@kgar.net>
2021-08-07 15:01:30 -0500

    through part of chapter 2

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,15 @@ struct State{
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();  // clear screen
-        ctx.print(1, 1, "hello RLTK!");
+
+        // get all position components and rederable components in the ECS
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        // go through and print all the things
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
@@ -36,8 +44,37 @@ fn main() -> rltk::BError {
         .with_title("Roguelike Tutorial")
         .build()?;
 
+    // game state
     let mut gs = State {
         ecs: World::new()
     };
+
+    // register component types
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    //add initial entity
+    gs.ecs.create_entity()
+      .with(Position { x: 40, y: 25 })
+      .with(Renderable {
+          glyph: rltk::to_cp437('@'),
+          fg: RGB::named(rltk::YELLOW),
+          bg: RGB::named(rltk::BLACK),
+      })
+      .build();
+
+    // add in some test things for now
+    for i in 1..10 {
+        gs.ecs.create_entity()
+          .with(Position { x: i * 7, y:20 })
+          .with(Renderable {
+              glyph: rltk::to_cp437('☺'),
+              fg: RGB::named(rltk::RED),
+              bg: RGB::named(rltk::BLACK),
+          })
+          .build();
+    }
+
+    // loop for-ev-er
     rltk::main_loop(context, gs)
 }
